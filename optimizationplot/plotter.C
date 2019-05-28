6d6943e83d7116508d046bce53ff90c486d20f64
asegura4488 <asegura4488@gmail.com>
2019-05-28 11:13:21 -0500

    optimization plots

--- optimizationplot/plotter.C
+++ optimizationplot/plotter.C
@@ -6,14 +6,41 @@ std::vector<Double_t> X;
 std::vector<Double_t> Y;
 std::vector<string> Label;
 std::vector<string> Xlabel;
+std::vector<string> input;
+
+Double_t XMin = 1000.;
+Double_t Min = 1000.;
+
+Double_t XMax = 0.;
 Double_t Max = 0.;
+
+
+
+Int_t Index = 8;
+Int_t IndexLabel = 2;
+
+std::map<int, int> colormap;
 
 void fill(){
 
+ input.push_back("TauLSP270");
+ input.push_back("TauLSP360"); 
+ input.push_back("TauLSP450");
+
+ input.push_back("LTauLSP270");
+ input.push_back("LTauLSP360"); 
+ input.push_back("LTauLSP450"); 
+
+ input.push_back("JetLSP270");
+ input.push_back("JetLSP360"); 
+ input.push_back("JetLSP450");
+
+ colormap[0] = 4;
+ colormap[1] = 2;
+ colormap[2] = 3;
+
  ifstream file;
-// file.open("data/TauLSP270.txt", ios::in);
-// file.open("data/TauLSP360.txt", ios::in);
- file.open("data/TauLSP450.txt", ios::in);
+ file.open(("data/"+input[Index]+".txt").c_str(), ios::in);
 
  Double_t x,y;
  while(!file.eof()){
@@ -35,11 +62,27 @@ void fill(){
  Xlabel.push_back("p_{#font[12]{T}}(j^{lead})[GeV]");
 }
 
-void GetMax(Double_t &Max_){
+void GetMax(Double_t &Max_, Double_t &XMax_){
 
   for(auto& it : Y){
   if(it > Max_) Max_ = it;
   }
+  for(auto& it : X){
+  if(it > XMax_) XMax_ = it;
+  }
+
+
+}
+
+void GetMin(Double_t &Min_, Double_t &XMin_){
+
+  for(auto& it : Y){
+  if(it < Min_) Min_ = it;
+  }
+  for(auto& it : X){
+  if(it < XMin_) XMin_ = it;
+  }
+
 
 }
 
@@ -50,7 +93,8 @@ void plotter(){
  std:cout << "pt:" << X[i] << " S: " << Y[i] << std::endl;
  }
 
- GetMax(Max); std::cout << " Max: " << Max <<std::endl;
+ GetMin(Min,XMin); std::cout << " Min: " << XMin << " " << Min << std::endl;
+ GetMax(Max,XMax); std::cout << " Max: " << XMax << " " << Max << std::endl;
    
  std::string Smax = to_string(Max);
  size_t tamaño= Smax.length(); 
@@ -95,17 +139,18 @@ void plotter(){
  gr->GetYaxis()->SetTitleOffset(1.0);
  gr->GetYaxis()->SetLabelSize(0.05);
 
- gr->GetYaxis()->SetRangeUser(0.,1.4);
+ gr->GetXaxis()->SetRangeUser(XMin-3,XMax+3);
+ gr->GetYaxis()->SetRangeUser(Min/Max-0.3,1.+0.3);
 
  gr->SetTitle("");
- gr->SetMarkerColor(4);
+ gr->SetMarkerColor(colormap[IndexLabel]);
  gr->SetMarkerStyle(20); 
  gr->SetMarkerSize(1.5);
  gr->Draw("AP");
 
- gROOT->ForceStyle();
- gStyle->SetLegendBorderSize(0);
-
+ 
+ 
+ 
  TLegend *leg = new TLegend(0.2621777,0.8004246,0.8868195,0.8832272,NULL,"brNDC");
  leg->SetBorderSize(0);
  leg->SetLineColor(1);
@@ -113,7 +158,7 @@ void plotter(){
  leg->SetLineWidth(1);
  leg->SetFillColor(0);
  leg->SetFillStyle(1001);
- TLegendEntry *entry=leg->AddEntry(gr,Label[1].c_str(),"p");
+ TLegendEntry *entry=leg->AddEntry(gr,Label[IndexLabel].c_str(),"p");
  entry->SetTextFont(42);
  leg->Draw();
 
@@ -121,10 +166,19 @@ void plotter(){
  TLatex latex;
  latex.SetTextSize(0.05);
  latex.SetTextFont(52);
- latex.DrawLatex(54.1,1.103929,("#bf{S_{max}}="+Smax).c_str());
+ //latex.DrawLatex(54.1,1.+0.01,("#bf{S_{max}}="+Smax).c_str());
+ latex.DrawLatex(140.,1.+0.1,("#bf{S_{max}}="+Smax).c_str());
 
  delete[] X_;
  delete[] Y_;
+ 
+ TLine *line = new TLine(XMin-3,1.0,XMax+3,1.0);
+ line->SetLineStyle(2);
+ line->SetLineWidth(2);
+ line->SetLineColor(kBlack);
+ line->Draw();
+
+ c1->SaveAs((input[Index]+".pdf").c_str());
 }
 
 int main(){
